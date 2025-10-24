64eddb1a56c6082d6cbc8d04ddd23f2ab449369c
lucas mateus <lucas.venanciorossi@outlook.com>
2025-10-24 16:05:11 -0300

    mudança de single para double coat

--- sistemlink/home/urls.py
+++ sistemlink/home/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
-    path('', views.Home, name='home'),
-    path('CalcMetro/', views.CalcMetro, name='calc_metro'),
-    path('CalcBobina/', views.CalcBobina, name='calc_bobina')
-
+urlpatterns = [
+    path("", views.Home, name="home"),
+    path("CalcMetro/", views.CalcMetro, name="calc_metro"),
+    path("CalcBobina/", views.CalcBobina, name="calc_bobina"),
 ]
