a09f705b72a9ca5224194ea2f7f4162e0803e286
lucas mateus <lucas.venanciorossi@outlook.com>
2025-10-28 11:32:06 -0300

    consertando os arquivos estaticos de produção

--- sistemlink/sistemalink/settings.py
+++ sistemlink/sistemalink/settings.py
@@ -113,9 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR.parent.parent, "www", "static")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR.parent.parent, "www", "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
